c553495c422a8809367f0e857a452e5ac99fb903
David Siegmund <dasi8976@bht-berlin.de>
2025-11-05 16:16:15 +0100

    merge commit se1-repo/b1-numbers

--- src/main/application/Application.java
+++ src/main/application/Application.java
@@ -1,11 +1,12 @@
 package application;
 
-import java.util.Arrays;
+import numbers.Numbers;
+
 
 /**
  * Application class with a {@code main()} - function that parses command line
  * arguments.
- *
+ * 
  * @version <code style=color:green>{@value application.package_info#Version}</code>
  * @author <code style=color:blue>{@value application.package_info#Author}</code>
  */
@@ -20,8 +21,12 @@ public class Application {
         var greeting = String.format(module==null? "%s, se1-play" : "%s, %s (modular)", "Hello", module);
         System.out.println(greeting);
 
-        Arrays.stream(args)
-            .map(arg -> String.format(" - arg: %s", arg))
-            .forEach(System.out::println);
+        // Arrays.stream(args)
+        //     .map(arg -> String.format(" - arg: %s", arg))
+        //     .forEach(System.out::println);
+
+        Numbers numbers = Numbers.getInstance();
+        Runner runner = Numbers.createRunner(numbers);
+        runner.run(args);
     }
 }
